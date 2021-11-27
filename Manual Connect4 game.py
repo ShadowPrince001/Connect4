3152fc791d57a60417a305db09ac3a5495c5970e
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-11-27 21:00:15 +0530

    Add files via upload

--- Manual Connect4 game.py
+++ Manual Connect4 game.py
@@ -15,15 +15,22 @@ for a in range(0,7):
     
 Column1 = pygame.draw.rect(screen, [0, 0, 0], [5, 50, 90, 540], 1)
 Column2 = pygame.draw.rect(screen, [0, 0, 0], [105, 50, 90, 540], 1)
-Column1 = pygame.draw.rect(screen, [0, 0, 0], [205, 50, 90, 540], 1)
-Column2 = pygame.draw.rect(screen, [0, 0, 0], [305, 50, 90, 540], 1)
-Column1 = pygame.draw.rect(screen, [0, 0, 0], [405, 50, 90, 540], 1)
-Column2 = pygame.draw.rect(screen, [0, 0, 0], [505, 50, 90, 540], 1)
-Column1 = pygame.draw.rect(screen, [0, 0, 0], [605, 50, 90, 540], 1)
-
-
-
+Column3 = pygame.draw.rect(screen, [0, 0, 0], [205, 50, 90, 540], 1)
+Column4 = pygame.draw.rect(screen, [0, 0, 0], [305, 50, 90, 540], 1)
+Column5 = pygame.draw.rect(screen, [0, 0, 0], [405, 50, 90, 540], 1)
+Column6 = pygame.draw.rect(screen, [0, 0, 0], [505, 50, 90, 540], 1)
+Column7 = pygame.draw.rect(screen, [0, 0, 0], [605, 50, 90, 540], 1)
+pygame.display.flip()
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                if Column1.collidepoint(event.pos):
+                    print("hi")
+                elif Column3.collidepoint(event.pos):                    
+                    run= False
+                    
 print(screen_array)
 
 pygame.display.flip()
-
