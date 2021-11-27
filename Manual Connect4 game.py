3d1f0b22030d25ec4f48b3a087d43bd3ecf06f30
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-11-27 16:05:14 +0530

    Update Manual Connect4 game.py

--- Manual Connect4 game.py
+++ Manual Connect4 game.py
@@ -9,15 +9,21 @@ pygame.display.set_caption("This is Connect4.")
 
 for a in range(0,7):
     for b in range(0,6):
-        h, k = 45+(a*100),90+(b*90)
+        h, k = 50+(a*100),90+(b*90)
         pygame.draw.circle(screen, (0, 0, 0), (h,k), 30)        
         screen_array= pygame.surfarray.pixels2d(screen)
-        for y in range(600):
-            for x in range(90):
-                screen_array[x,y]=0
-     
+    
+Column1 = pygame.draw.rect(screen, [0, 0, 0], [5, 50, 90, 540], 1)
+Column2 = pygame.draw.rect(screen, [0, 0, 0], [105, 50, 90, 540], 1)
+Column1 = pygame.draw.rect(screen, [0, 0, 0], [205, 50, 90, 540], 1)
+Column2 = pygame.draw.rect(screen, [0, 0, 0], [305, 50, 90, 540], 1)
+Column1 = pygame.draw.rect(screen, [0, 0, 0], [405, 50, 90, 540], 1)
+Column2 = pygame.draw.rect(screen, [0, 0, 0], [505, 50, 90, 540], 1)
+Column1 = pygame.draw.rect(screen, [0, 0, 0], [605, 50, 90, 540], 1)
+
+
+
 print(screen_array)
 
 pygame.display.flip()
-
 
