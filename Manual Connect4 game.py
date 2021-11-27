d261bf88889657e1968e2975ecdfd4bd13f8e3dc
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-11-27 13:34:12 +0530

    Add files via upload

--- Manual Connect4 game.py
+++ Manual Connect4 game.py
@@ -3,20 +3,17 @@ import numpy
 pygame.init()
 size = [700, 600]
 screen = pygame.display.set_mode(size)
-bg = (0, 128, 255)
-screen.fill(bg)
-pygame.display.set_caption("This is Connect4.")
-
+color=(255,255,255)
+screen.fill(color)
 for a in range(0,7):
     for b in range(0,6):
         h, k = 45+(a*100),105+(b*90)
-        pygame.draw.circle(screen, (0, 0, 0), (h,k), 30)        
         screen_array= pygame.surfarray.pixels2d(screen)
-        for y in range(700):
-            for x in range(600):
+        for y in range(200):
+            for x in range(100):
                 for r in range(30):
-                    if abs((x-h)**2 + (y-k)**2 - r**2) ==0:
-                        screen_array[x,y]=255
+                    if round(float((x-h)**2 + (y-k)**2 - r**2)) ==0:
+                        screen_array[x,y]=0
 print(screen_array)
 
 pygame.display.flip()
